86e0f34f95602dc1c7f9cd3c458421dd3bdb1c65
Abdallah Bari <abdellah01win@gmail.com>
2025-02-27 21:34:21 +0000

    Refactor & update login and register schemas

--- src/pages/auth/components/index.ts
+++ src/pages/auth/components/index.ts
@@ -1,2 +1,18 @@
-export * from './LoginSchema'
-export * from './RegisterSchema'
+import { z } from '@/plugins'
+import { toTypedSchema } from '@vee-validate/zod'
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8)
+})
+const registerSchema = z.object({
+  name: z.string().min(3),
+  email: z.string().email(),
+  password: z.string().min(8)
+})
+
+export type RegisterForm = z.infer<typeof registerSchema>
+export type LoginForm = z.infer<typeof loginSchema>
+
+export const logintypedSchema = toTypedSchema(loginSchema)
+export const registertypedSchema = toTypedSchema(registerSchema)
